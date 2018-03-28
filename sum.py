32aa429c2572307bf1efe4ef3b92e0004a65e63f
RossY <ross@famyelland.net>
2018-03-28 15:49:59 +1100

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+#Program to get and add two numbers together
+#
+#get first number.  note no checking for number type.
+a = input("first number? ")
+# get first number
+b = input("second number? ")
+#convert numbers to numbers and add.  
+sum = float(a) + float(b)
+print ("Sum is",sum)
